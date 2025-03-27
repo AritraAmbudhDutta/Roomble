c28811f2eb13b8eb34e7ccb73dd2e5c9ca6192ff
Bikramjeet Singh <166037831+bikkygill007@users.noreply.github.com>
2025-03-27 15:59:13 +0530

    Update AddProperty.jsx

--- frontend/src/components/AddProperty.jsx
+++ frontend/src/components/AddProperty.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DragAndDrop from './AddPropertyComponents/DragAndDrop';
 import "../css/AddPropertyStyles/AddProperty.css";
 import { useNavigate } from "react-router-dom";
