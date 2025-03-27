33e33d6586cf04a927be057ea7a8e3b828a3739a
Shlok Jain <85099963+Shlok-Jain@users.noreply.github.com>
2025-03-27 13:13:12 +0530

    Added error handling in Basestate

--- frontend/src/context/base/Basestate.jsx
+++ frontend/src/context/base/Basestate.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Basecontext } from "./Basecontext";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"; // Import the CSS for styling
@@ -6,34 +6,46 @@ import { useNavigate } from "react-router-dom";
 
 
 const BaseState = (props) => {
-    const [user, setUser] = useState({"type":"none"});
+    const [user, setUser] = useState({ "type": "none" });
     const navigate = useNavigate();
-    
+    const [somethingwentwrong, setsomethingwentwrong] = useState(false);
+
     const fetuser = async () => {
-        if(localStorage.getItem("authtoken") && user.type === "none"){
-            const res = await fetch("http://localhost:3000/api/auth/user", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "authtoken": localStorage.getItem("authtoken")
-                },
-            });
-            const data = await res.json();
-            // console.log(data.user)
-            if(data.success){
-                setUser(data.user);
-            }
-            else{
-                toast.error("Please Login again.");
-                localStorage.removeItem("authtoken");
-                setUser({"type":"none"});
-                navigate("/login");
+        try {
+            if (localStorage.getItem("authtoken") && user.type === "none") {
+                const res = await fetch("http://localhost:3000/api/auth/user", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "authtoken": localStorage.getItem("authtoken")
+                    },
+                });
+                const data = await res.json();
+                // console.log(data.user)
+                if (data.success) {
+                    setUser(data.user);
+                }
+                else {
+                    setsomethingwentwrong(true);
+                }
             }
         }
-            
+        catch (err) {
+            console.log(err);
+            setsomethingwentwrong(true);
+        }
+
     }
+    useEffect(() => {
+        if (somethingwentwrong) {
+            toast.error("Please Login again");
+            localStorage.removeItem("authtoken");
+            setUser({ "type": "none" });
+            navigate("/login");
+        }
+    }, [somethingwentwrong])
     return (
-        <Basecontext.Provider value={ {user, setUser, fetuser} }>
+        <Basecontext.Provider value={{ user, setUser, fetuser }}>
             {props.children}
         </Basecontext.Provider>
     );
